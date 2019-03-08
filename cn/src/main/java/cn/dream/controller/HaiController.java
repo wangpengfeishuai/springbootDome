e67569d4fae3d41d3dd9706fed7843efa876cd8e
pf_glb <847797764@qq.com>
2019-03-08 14:37:18 +0800

    取消定时任务

--- cn/src/main/java/cn/dream/controller/HaiController.java
+++ cn/src/main/java/cn/dream/controller/HaiController.java
@@ -2,10 +2,7 @@ package cn.dream.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
--- cn/src/main/java/cn/dream/utils/ScheduledService.java
+++ cn/src/main/java/cn/dream/utils/ScheduledService.java
@@ -1,27 +1,31 @@
 package cn.dream.utils;
 
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * @author xx
+ *
+ */
 @Slf4j
 @Component
 public class ScheduledService {
 
-	@Scheduled(cron = "0/5 * * * * *")
+//	@Scheduled(cron = "0/5 * * * * *")
 	public void scheduled() {
-		
-		System.out.println("this is scheduled" );
+
+		System.out.println("this is scheduled");
 	}
 
-	@Scheduled(fixedRate = 5000)
+//	@Scheduled(fixedRate = 5000)
 	public void scheduled1() {
-		System.out.println("=====>>>>>使用fixedRate{}"+ System.currentTimeMillis());
+		System.out.println("=====>>>>>使用fixedRate{}" + System.currentTimeMillis());
 	}
 
-	@Scheduled(fixedDelay = 5000)
+//	@Scheduled(fixedDelay = 5000)
 	public void scheduled2() {
-		System.out.println("=====>>>>>fixedDelay{}"+ System.currentTimeMillis());
+		System.out.println("=====>>>>>fixedDelay{}" + System.currentTimeMillis());
 	}
+
 }
