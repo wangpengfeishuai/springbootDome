460ef8beb7ec3a767a1e448e4327738ac0566987
pf_glb <847797764@qq.com>
2019-03-28 14:59:33 +0800

    测试

---
+++ cn/src/test/java/cn/TestJavaData.java
@@ -0,0 +1,14 @@
+package cn;
+
+import org.junit.Test;
+
+public class TestJavaData {
+	int b=10;
+	@Test
+	public void test1() {
+		int a = 100;
+		System.out.println(a);
+		System.out.println(++b);
+	}
+
+}
