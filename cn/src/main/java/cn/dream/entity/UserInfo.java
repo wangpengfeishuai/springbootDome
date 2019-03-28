4f962cdec389917fec8df246d3f46f377c36ea5f
pf_glb <847797764@qq.com>
2019-03-28 15:28:46 +0800

    测试合并冲突

--- cn/src/main/java/cn/dream/entity/UserInfo.java
+++ cn/src/main/java/cn/dream/entity/UserInfo.java
@@ -6,7 +6,7 @@ package cn.dream.entity;
  */
 public class UserInfo {
 	private String userAccount;
-	private String userName;
+	private String userNamex;
 	private String age;
 	private String sex;
 	public String getUserAccount() {
@@ -15,11 +15,11 @@ public class UserInfo {
 	public void setUserAccount(String userAccount) {
 		this.userAccount = userAccount;
 	}
-	public String getUserName() {
-		return userName;
+	public String getUserNamex() {
+		return userNamex;
 	}
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setUserNamex(String userNamex) {
+		this.userNamex = userNamex;
 	}
 	public String getAge() {
 		return age;
